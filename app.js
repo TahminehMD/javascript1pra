c714a4814d62897dbff480a12f3176afaca1f165
tahmineh darvish <tahmineh5314@gmail.com>
2023-01-15 20:33:36 -0800

    the first apps

---
+++ app.js
@@ -0,0 +1,8 @@
+// this lines for input
+const prompt = require('prompt-sync')()
+const name = prompt("what is your name ? ")
+
+
+// this is a comment
+// this line for output
+console.log("hello java script")
